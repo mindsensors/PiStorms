a66032b9ea1a2b5b95327c327b995e57212715a3
mindsensors <info@mindsensors.com>
2016-06-29 17:01:59 -0400

    new file added

---
+++ programs/04-demos/00-TakeSelfie.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2015 mindsensors.com
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+#mindsensors.com invests time and resources providing this open source code, 
+#please support mindsensors.com  by purchasing products from mindsensors.com!
+#Learn more product option visit us @  http://www.mindsensors.com/
+#
+# History:
+# Date      Author      Comments
+# June 2016 Yug Rao     Initial Authoring
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import os,sys,inspect,time
+from subprocess import call
+import cv2
+import imutils
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+from PiStorms import PiStorms
+psm = PiStorms()
+
+picam = PiCamera()
+exitNow = 0
+time.sleep(.2)
+while not exitNow:
+	psm.screen.termPrintAt(8, "Click Go to take a picture")
+	if(psm.isKeyPressed()):
+		picam.capture('/tmp/pic.jpg')
+		psm.screen.fillBmp(0,0,320,240,path = "/tmp/pic.jpg")
+		psm.screen.termPrintAt(8, "Captured!")
+		time.sleep(1)
+		psm.screen.termPrintAt(9, "Touch the screen to exit")
+
+	if (psm.screen.checkButton(0,0,320,320)):
+			psm.screen.clearScreen()
+			psm.screen.termPrintAt(9,"Exiting to menu")
+			time.sleep(0.5) 
+			exitNow = True 
+
+
