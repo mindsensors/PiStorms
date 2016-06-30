2e6523006521079a0fcabb55c3e1b6c8d2581761
mindsensors <info@mindsensors.com>
2016-06-30 16:11:03 -0400

    library supporting screen capture

---
+++ sys/MS_ILI9341.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2016 mindsensors.com
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+#mindsensors.com invests time and resources providing this open source code, 
+#please support mindsensors.com  by purchasing products from mindsensors.com!
+#Learn more product option visit us @  http://www.mindsensors.com/
+#
+# History:
+# Date         Author          Comments
+# June 2016    Roman Bohuk     Initial Authoring 
+
+import Adafruit_ILI9341
+import os
+import datetime
+
+class ILI9341(Adafruit_ILI9341.ILI9341):
+    def __init__(self, dc, spi, rst=None, gpio=None, width=Adafruit_ILI9341.ILI9341_TFTWIDTH,
+        height=Adafruit_ILI9341.ILI9341_TFTHEIGHT):
+        Adafruit_ILI9341.ILI9341.__init__(self, dc, spi, rst, gpio, width,
+        height)
+        self.record_path = "/tmp/pistormsrecord"
+
+    def save(self, path=None, img=None, extension="PNG"):
+        """Writes the buffer to a file"""
+        # If no path is specified, store the file in the current folder with timestamp
+        tstamp = datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S%f')[:-3:]
+        path = "/var/tmp/ps_images/%s.%s" % (tstamp, extension) if path is None else path
+        # If no PIL Image specified, store the whole screen
+        dat = self.getBuffer() if img is None else img
+        # Save to file
+        dat.save(path, extension)
+
+    def getBuffer(self):
+        """Gets the copy of a bugger"""
+        return self.buffer.copy().rotate(270)
+
+    def getSectionBounds(self, left, upper, right, lower):
+        """Gets the section of the screen"""
+        return self.getBuffer().crop((left, upper, right, lower))
+
+    def getSection(self, x, y, width, height):
+        """Gets the section of the screen with same parameters as mindensorsUI"""
+        return self.get_section_bounds(x, y, x + width, y + height)
+
+    def getPixel(self, x, y):
+        """Gets the RGB of a pixel"""
+        return self.getBuffer().getpixel((x, y))
+    
+    def recordFileExists(self):
+        return os.path.isfile(self.record_path)
+        
+    def startRecording(self, frames="-"):
+        with open(self.record_path, "w+") as f: f.write(frames)
+    
+    def stopRecording(self):
+        if self.recordFileExists(): os.remove(self.record_path)
+    
+    def readRecordingCount(self):
+        if self.recordFileExists():
+            with open(self.record_path, "r") as f:
+                return f.read()
+        else: return ""
+    
+    def isTakingFrames(self, fileContents):
+        return fileContents == "-" or fileContents.isdigit()
+    
+    def decrementRecordingCount(self, fileContents):
+        toWrite = ""
+        if fileContents == "-": toWrite = "-"
+        elif fileContents.isdigit():
+            temp = int(fileContents)-1
+            toWrite = str(temp) if temp > 0 else ""
+        if toWrite == "": self.stopRecording()
+        else: self.startRecording(toWrite)
+    
+    def display(self, image=None):
+        content = self.readRecordingCount()
+        if self.isTakingFrames(content):
+            self.decrementRecordingCount(content)
+            self.save()
+        if image is None: image = self.buffer
+        self.set_window()
+        pixelbytes = list(Adafruit_ILI9341.image_to_data(image))
+        self.data(pixelbytes)
