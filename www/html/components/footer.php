edfc9958da538561fedea6869f445b227b19df3c
mindsensors <info@mindsensors.com>
2016-07-12 15:23:23 -0400

    web interface update

---
+++ www/html/components/footer.php
@@ -0,0 +1,7 @@
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Software Version:</b> <span class="software_version"><i class="fa fa-refresh fa-spin"></i> fetching</span> / <b>Hardware Version:</b> <span class="firmware_version"><i class="fa fa-refresh fa-spin"></i> fetching</span>
+    </div>
+    <strong>PiStorms by <a href="http://mindsensors.com" target="_blank">mindsensors.com</a></strong>
+  </footer>
+  
