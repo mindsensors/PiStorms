efbc20e94a2e6a54d9ac6dba38fc93a66ccdd1c8
Seth T <seth10@users.noreply.github.com>
2017-04-14 14:13:29 -0400

    Make example a function (passes vertical line test)
    Generating x list on the fly. Tried using enumerate but that didn't work. Recreating the range each iteration probably isn't the most efficient.

--- programs/00-DataVisualization/01-LiveUpdating.py
+++ programs/00-DataVisualization/01-LiveUpdating.py
@@ -6,17 +6,16 @@ import tempfile
 from PiStorms import PiStorms
 
 plt.figure(figsize=(4,3), dpi=80)
-x,y = [],[]
+y = []
 psm = PiStorms()
 image = tempfile.NamedTemporaryFile()
 while True:
-    x.append(np.random.random())
     y.append(np.random.random())
     plt.cla() # clear axis
     plt.xlabel('time (s)')
     plt.ylabel('voltage (mV)')
     plt.title('About as simple as it gets, folks')
     plt.grid(True)
-    plt.plot(x, y)
+    plt.plot(range(0,len(y)), y)
     plt.savefig(image.name+".png")
     psm.screen.fillBmp(0,0, 320,240, image.name+".png")
