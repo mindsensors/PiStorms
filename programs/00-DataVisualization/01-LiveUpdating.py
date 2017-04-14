fe0f87359b0786eee147fb994e1fe52cff54060f
Seth T <seth10@users.noreply.github.com>
2017-04-14 14:10:59 -0400

    Random squiggle
    Also goes backwards (not a function)

--- programs/00-DataVisualization/01-LiveUpdating.py
+++ programs/00-DataVisualization/01-LiveUpdating.py
@@ -6,16 +6,17 @@ import tempfile
 from PiStorms import PiStorms
 
 plt.figure(figsize=(4,3), dpi=80)
-t = np.arange(0.0, 2.0, 0.01)
+x,y = [],[]
 psm = PiStorms()
 image = tempfile.NamedTemporaryFile()
 while True:
-    s = map(lambda n: np.random.random(), t)
+    x.append(np.random.random())
+    y.append(np.random.random())
     plt.cla() # clear axis
     plt.xlabel('time (s)')
     plt.ylabel('voltage (mV)')
     plt.title('About as simple as it gets, folks')
     plt.grid(True)
-    plt.plot(t, s)
+    plt.plot(x, y)
     plt.savefig(image.name+".png")
     psm.screen.fillBmp(0,0, 320,240, image.name+".png")
