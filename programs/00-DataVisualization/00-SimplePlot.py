ffa02eb411ff5d1d5b31e72807917cd91fa01748
Seth T <seth10@users.noreply.github.com>
2017-04-14 12:53:03 -0400

    Plotting without X11 required
    Previously I had to have an SSH session open with X11 forwarding for this to work, even though the output was to a file and thereby the PiStorms screen, not my screen.

--- programs/00-DataVisualization/00-SimplePlot.py
+++ programs/00-DataVisualization/00-SimplePlot.py
@@ -1,23 +1,26 @@
 import matplotlib
-matplotlib.use("GTK")
+matplotlib.use("AGG")
 import matplotlib.pyplot as plt
 import numpy as np
 
-t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2*np.pi*t)
-plt.plot(t, s)
-
+# setup figure
+plt.figure(figsize=(4,3), dpi=80)
 plt.xlabel('time (s)')
 plt.ylabel('voltage (mV)')
 plt.title('About as simple as it gets, folks')
 plt.grid(True)
 
+# generate and plot data
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2*np.pi*t)
+plt.plot(t, s)
+
+# save to temporary file
 import tempfile
 image = tempfile.NamedTemporaryFile()
-plt.savefig(image.name+".png", dpi=40)
+plt.savefig(image.name+".png")
 
-from PIL import Image
-Image.open(image.name+".png").save(image.name+".bmp")
+# draw on screen
 from PiStorms import PiStorms
 psm = PiStorms()
-psm.screen.fillBmp(0,0, 320,240, image.name+".bmp")
+psm.screen.fillBmp(0,0, 320,240, image.name+".png")
