7ebb2f8165b05e8632a6e3af254dd2d02a831e31
Seth T <seth10@users.noreply.github.com>
2017-04-14 14:28:52 -0400

    Show only last 10 data points, static y-axis range

--- programs/00-DataVisualization/01-LiveUpdating.py
+++ programs/00-DataVisualization/01-LiveUpdating.py
@@ -11,11 +11,13 @@ psm = PiStorms()
 image = tempfile.NamedTemporaryFile()
 while True:
     y.append(np.random.random())
+    y = y[-10:]
     plt.cla() # clear axis
     plt.xlabel('time (s)')
     plt.ylabel('voltage (mV)')
     plt.title('About as simple as it gets, folks')
     plt.grid(True)
+    plt.ylim((0,1))
     plt.plot(range(0,len(y)), y)
     plt.savefig(image.name+".png")
     psm.screen.fillBmp(0,0, 320,240, image.name+".png")
