3d3d7b8a8c7724a7bdab787f7f88c8a8d9ad0e3c
Seth T <seth10@users.noreply.github.com>
2017-04-14 14:52:22 -0400

    Redraw the figure less

--- programs/00-DataVisualization/01-LiveUpdating.py
+++ programs/00-DataVisualization/01-LiveUpdating.py
@@ -6,18 +6,20 @@ import tempfile
 from PiStorms import PiStorms
 
 plt.figure(figsize=(4,3), dpi=80)
+plt.xlabel('index')
+plt.ylabel('random data')
+plt.title('Live updating example')
+plt.grid(True)
+plt.ylim((0,1))
 y = np.zeros(10)
+plt.plot(range(0,len(y)), y)
+line = plt.gca().lines[0] # gca means "get current axis"
+
 psm = PiStorms()
 image = tempfile.NamedTemporaryFile()
 while True:
     y = np.roll(y, -1)
     y[y.size-1] = np.random.random()
-    plt.cla() # clear axis
-    plt.xlabel('time (s)')
-    plt.ylabel('voltage (mV)')
-    plt.title('About as simple as it gets, folks')
-    plt.grid(True)
-    plt.ylim((0,1))
-    plt.plot(range(0,len(y)), y)
+    line.set_ydata(y)
     plt.savefig(image.name+".png")
     psm.screen.fillBmp(0,0, 320,240, image.name+".png")
