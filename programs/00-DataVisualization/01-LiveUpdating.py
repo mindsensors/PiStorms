cdc847c9a7d189695e90736bf0f5c10cfe1aeb6d
Seth T <seth10@users.noreply.github.com>
2017-04-14 14:03:38 -0400

    Redraw titles and use random from numpy
    `plt.cla()` was also clearing the titles

--- programs/00-DataVisualization/01-LiveUpdating.py
+++ programs/00-DataVisualization/01-LiveUpdating.py
@@ -2,21 +2,20 @@ import matplotlib
 matplotlib.use("AGG")
 import matplotlib.pyplot as plt
 import numpy as np
-import tempfile, random
+import tempfile
 from PiStorms import PiStorms
 
 plt.figure(figsize=(4,3), dpi=80)
-plt.xlabel('time (s)')
-plt.ylabel('voltage (mV)')
-plt.title('About as simple as it gets, folks')
-plt.grid(True)
-
 t = np.arange(0.0, 2.0, 0.01)
 psm = PiStorms()
 image = tempfile.NamedTemporaryFile()
 while True:
-    s = map(lambda n: random.randint(1,100), t)
+    s = map(lambda n: np.random.random(), t)
     plt.cla() # clear axis
+    plt.xlabel('time (s)')
+    plt.ylabel('voltage (mV)')
+    plt.title('About as simple as it gets, folks')
+    plt.grid(True)
     plt.plot(t, s)
     plt.savefig(image.name+".png")
     psm.screen.fillBmp(0,0, 320,240, image.name+".png")
