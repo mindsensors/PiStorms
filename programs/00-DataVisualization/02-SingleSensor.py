4f7ed46617aca9307c296f4ea3dd036374d5cdbd
Seth T <seth10@users.noreply.github.com>
2017-04-14 17:01:42 -0400

    Create 02-SingleSensor.py

---
+++ programs/00-DataVisualization/02-SingleSensor.py
@@ -0,0 +1,30 @@
+import matplotlib
+matplotlib.use("AGG")
+import matplotlib.pyplot as plt
+import numpy as np
+import tempfile
+from PiStorms import PiStorms
+from LegoDevices import EV3InfraredSensor, PS_SENSOR_MODE_EV3_IR_PROXIMITY
+
+plt.figure(figsize=(4,3), dpi=80)
+plt.xlabel('time')
+plt.ylabel('reading')
+plt.title('Distance')
+plt.grid(True)
+plt.ylim((0,100))
+
+data = np.zeros(10)
+plt.plot(data)
+line = plt.gca().lines[0] # gca means "get current axis"
+
+psm = PiStorms()
+ir = EV3InfraredSensor("BAS1")
+ir.setMode(PS_SENSOR_MODE_EV3_IR_PROXIMITY)
+image = tempfile.NamedTemporaryFile()
+
+while True:
+    data = np.roll(data, -1)
+    data[-1] = ir.readProximity()
+    line.set_ydata(data)
+    plt.savefig(image.name+".png")
+    psm.screen.fillBmp(0,0, 320,240, image.name+".png")
