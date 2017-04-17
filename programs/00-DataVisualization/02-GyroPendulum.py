9b1c5fa1ce1d126268768c0c66d89e79760ccc34
Seth T <seth10@users.noreply.github.com>
2017-04-17 14:32:39 -0400

    Live updating graph with fast sensor readings
    Uses a thread to capture data and redraws the screen on the main thread.
    This way there are many more readings than one every 2.5 seconds.

--- programs/00-DataVisualization/02-GyroPendulum.py
+++ programs/00-DataVisualization/02-GyroPendulum.py
@@ -5,6 +5,7 @@ import numpy as np
 import tempfile
 from PiStorms import PiStorms
 from mindsensors import ABSIMU
+import threading, time
 
 plt.figure(figsize=(4,3), dpi=80)
 plt.xlabel('time')
@@ -15,16 +16,20 @@ plt.grid(True)
 psm = PiStorms()
 imu = ABSIMU()
 psm.BAS1.activateCustomSensorI2C()
-psm.screen.drawAutoText("Recording data!",   45, 40,  size=32)
-psm.screen.drawAutoText("Press GO to stop.", 65, 120, size=24)
 
 data = np.empty(0)
-while not psm.isKeyPressed():
-    accel = imu.get_accely()
-    if not accel > 30000:
-        data = np.append(data, accel)
-plt.plot(data)
 
-image = tempfile.NamedTemporaryFile()
-plt.savefig(image.name, format="png")
-psm.screen.fillBmp(0,0, 320,240, image.name)
+def captureData():
+    global data
+    while not psm.isKeyPressed():
+        accel = imu.get_accely()
+        if not accel > 30000:
+            data = np.append(data, accel)
+        time.sleep(0.01)
+threading.Thread(target=captureData).start()
+
+while not psm.isKeyPressed():
+    plt.plot(data, color="blue")
+    image = tempfile.NamedTemporaryFile()
+    plt.savefig(image.name, format="png")
+    psm.screen.fillBmp(0,0, 320,240, image.name)
