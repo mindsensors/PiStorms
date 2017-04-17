0fdb4ef0e5891763686734a2c036c96cf9e7687e
Seth T <seth10@users.noreply.github.com>
2017-04-17 14:12:33 -0400

    Use AbsoluteIMU instead of EV3 Gyro
    Acceleration is really what we want to capture.

--- programs/00-DataVisualization/02-GyroPendulum.py
+++ programs/00-DataVisualization/02-GyroPendulum.py
@@ -4,23 +4,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tempfile
 from PiStorms import PiStorms
-from LegoDevices import EV3GyroSensor, PS_SENSOR_MODE_EV3_GYRO_RATE, PS_SENSOR_MODE_EV3_GYRO_ANGLE
+from mindsensors import ABSIMU
 
 plt.figure(figsize=(4,3), dpi=80)
 plt.xlabel('time')
-plt.ylabel('angle')
-plt.title('Gyro Pendulum')
+plt.ylabel('acceleration')
+plt.title('AbsoluteIMU Pendulum')
 plt.grid(True)
 
 psm = PiStorms()
-gyro = EV3GyroSensor("BAS1")
-gyro.setMode(PS_SENSOR_MODE_EV3_GYRO_RATE)
+imu = ABSIMU()
+psm.BAS1.activateCustomSensorI2C()
 psm.screen.drawAutoText("Recording data!",   45, 40,  size=32)
 psm.screen.drawAutoText("Press GO to stop.", 65, 120, size=24)
 
 data = np.empty(0)
 while not psm.isKeyPressed():
-    data = np.append(data, gyro.readValue())
+    accel = imu.get_accely()
+    if not accel > 30000:
+        data = np.append(data, accel)
 plt.plot(data)
 
 image = tempfile.NamedTemporaryFile()
