985b7cae96a13dbd374d7aad216fb5229802c9e2
Seth T <seth10@users.noreply.github.com>
2017-04-17 17:40:26 -0400

    Save data to file and make sure thread stops
    Because the captureData thread is repeating its loop more quickly than
    the plotting loop, it is better suited to check for GO button presses.

--- programs/00-DataVisualization/02-Pendulum.py
+++ programs/00-DataVisualization/02-Pendulum.py
@@ -17,7 +17,7 @@ psm = PiStorms()
 imu = ABSIMU()
 psm.BAS1.activateCustomSensorI2C() # see example in 50-SensorDemos
 
-data = np.empty(0) # data starts completely empty
+data = np.empty(0, dtype="int_") # data starts completely empty (signed integer data type)
 
 # We could take the same approach as the previous examples, but unfortunately
 # the process of generating the graph image, saving it, and drawing it to the screen
@@ -30,16 +30,20 @@ data = np.empty(0) # data starts completely empty
 # It stores these values in the data variable. Meanwile (further down) a loop
 # will continue to re-plot the data as it is updated and show it on the screen.
 
+stop = False
 def captureData():
-    global data # share the data variable from the global namespace
+    global data, stop # share the data and stop variables from the global namespace
     while not psm.isKeyPressed():
-        accel = imu.get_accely() # acceleration in the y direction
+        accel = imu.get_accely()+imu.get_accelx() # acceleration in the y direction
         if not accel > 30000: # as long as it's not a crazy value...
             data = np.append(data, accel) # add it to the data array
         time.sleep(0.01) # take a short break to let the Pi do the other things it needs to
+    stop = True
+    np.savetxt("pendulum.csv", data, delimiter=",", fmt="%i")
+
 threading.Thread(target=captureData).start() # create a new thread that will run this method and start it
 
-while not psm.isKeyPressed():
+while not stop:
     plt.plot(data, color="blue")
     image = tempfile.NamedTemporaryFile()
     plt.savefig(image.name, format="png")
