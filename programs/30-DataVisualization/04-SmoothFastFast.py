98f99221f234217a64465013a0191e9514fcafb3
Seth T <seth10@users.noreply.github.com>
2017-05-03 15:58:27 -0400

    Working comprehensive example
    Using a lock is causing data to be recorded very slowly. There were
    problems with the drawing loop calling axis.lines.pop() while the data
    loop tried to .set_ydata on that line.

--- programs/30-DataVisualization/04-SmoothFastFast.py
+++ programs/30-DataVisualization/04-SmoothFastFast.py
@@ -40,16 +40,19 @@ from PIL import Image
 from PiStorms import PiStorms
 from mindsensors import ABSIMU
 
+DATA_SIZE = 40 # only the latest n data points will be shown on screen (this is not a cap on how much data will be recorded in total)
+
 plt.figure(figsize=(4,3), dpi=80)
 plt.xlabel('time')
 plt.ylabel('tilt')
 plt.title('3-Axis AbsoluteIMU Tilt')
 plt.grid(True)
 
-data = np.zeros([3,0], dtype="int_")
+data = np.zeros([3,DATA_SIZE], dtype="int_")
 plt.plot(data.T) # transpose
 axis = plt.gca() # get current axis
 axis.set_xticklabels([]) # hide x-axis tick labels
+axis.set_color_cycle(['red', 'green', 'blue'])
 
 psm = PiStorms()
 imu = ABSIMU()
@@ -58,30 +61,32 @@ psm.BAS1.activateCustomSensorI2C()
 canvas = plt.get_current_fig_manager().canvas
 disp = psm.screen.disp
 
+lock = threading.Lock()
 stop = False
 def captureData():
     global data, stop, axis
     while len(axis.lines) < 3: time.sleep(0.1) # wait for the three lines to exist
     while not psm.isKeyPressed():
         tilt = imu.get_tiltall()[0] # read the x, y, and z tilt data
-        for i in range(3): # update the data array and graph line for each axis
-            data[i] = np.append(data[i], tilt[i])
+        data = np.column_stack([data, tilt])
+        lock.acquire()
+        for i in range(3): # update the graph line for each axis
             axis.lines[i].set_ydata(data[i])
+        lock.release()
         time.sleep(0.01)
     stop = True
 threading.Thread(target=captureData).start()
 
-while len(data[0]) < 1: time.sleep(0.1) # wait for the first data points before plotting
-while not stop:
-    if axis.lines:
-        for i in range(3): axis.lines.pop()
-    smooth_x = np.linspace(0, len(data[0])-1, 320)
-    for i in range(3): plt.plot(smooth_x, spline(np.arange(len(data[i])), data[i], smooth_x))
-    axis.relim() # recompute axis limits/bounds
-    axis.autoscale_view()
+while not stop and not psm.isKeyPressed():
+    lock.acquire()
+    for i in range(3): axis.lines.pop()
+    slicedata = data[:,-1*DATA_SIZE:]
+    smooth_x = np.linspace(0, len(slicedata[0])-1, 320)
+    for i in range(3): plt.plot(smooth_x, spline(np.arange(len(slicedata[i])), slicedata[i], smooth_x))
     canvas.draw()
+    lock.release()
     disp.buffer = Image.frombytes('RGB', canvas.get_width_height(), canvas.tostring_rgb()).rotate(-90*psm.screen.currentRotation)
     disp.display()
 
-plt.savefig("/home/pi/Documents/smoothfastfast.png")
-np.savetxt("/home/pi/Documents/smoothfastfast.csv", data, delimiter=",", fmt="%i")
+#plt.savefig("/home/pi/Documents/smoothfastfast.png")
+np.savetxt("/home/pi/Documents/smoothfastfast.csv", data.T[DATA_SIZE:], fmt="%i")
