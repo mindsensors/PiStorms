d681c2249031185c57ef98eb2f67110dc0e81017
Seth T <seth10@users.noreply.github.com>
2017-05-12 09:28:20 -0400

    Delete login functionality
    There was no /var/tmp/web/web_passcode.txt so any passcode was accepted,
    and there is no "WebKey script in utils folder"

--- www/html/api/downloadimages.php
+++ www/html/api/downloadimages.php
@@ -1,10 +1,6 @@
 <?php
 include "config.php";
 
-if (!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
-    die();
-}
-
 $zip = new ZipArchive;
 $download = 'screenshots_export.zip';
 $zip->open($download, ZipArchive::CREATE);
