959fcfff08968218bb473e3b136ea93a2eb457ef
Seth T <seth10@users.noreply.github.com>
2017-06-13 17:13:41 -0400

    Improve striaght line program

--- programs_grx/00-StraightLine.py
+++ programs_grx/00-StraightLine.py
@@ -10,24 +10,21 @@ rightServo = RCServo("BBM1", 1300)
 leftEncoder = GroveDigitalPort("BBD2", GRXCom.TYPE.ENCODER) # no mode, not associated with any servo
 rightEncoder = GroveDigitalPort("BAD1", GRXCom.TYPE.ENCODER)
 
-GRXCom.I2C.A.writeByte(GRXCom.REGISTER.COMMAND, GRXCom.COMMAND.RESET_ENCODERS)
-GRXCom.I2C.B.writeByte(GRXCom.REGISTER.COMMAND, GRXCom.COMMAND.RESET_ENCODERS)
-time.sleep(0.1)
+for bank in [GRXCom.I2C.A, GRXCom.I2C.B]: bank.writeByte(GRXCom.REGISTER.COMMAND, GRXCom.COMMAND.RESET_ENCODERS)
 
-def lEnc(): return -1*leftEncoder.comm.readEncoderValue()
-def rEnc(): return rightEncoder.comm.readEncoderValue()
-
-for i in range(20):
+for i in range(200):
     try:
-        print "{:6d} {:6d}".format(lEnc(), rEnc())
-        if lEnc() < rEnc():
+        leftEncoderValue = -1*leftEncoder.comm.readEncoderValue()
+        rightEncoderValue = rightEncoder.comm.readEncoderValue()
+
+        print "{:6d} {:6d}".format(leftEncoderValue, rightEncoderValue)
+        if leftEncoderValue < rightEncoderValue:
             leftServo.setSpeed(30)
             rightServo.setSpeed(50)
         else:
             leftServo.setSpeed(50)
             rightServo.setSpeed(30)
-        time.sleep(0.1)
-    except: pass
+    except TypeError: pass # I2C fail
 
 leftServo.stop()
 rightServo.stop()
