4a78ab68c38b4c25bda0bd256e8ccc9dca643b8a
Seth T <seth10@users.noreply.github.com>
2017-06-27 15:28:52 +0000

    Correct lock file removal when stopping MSBrowser

--- setup/MSBrowser.sh
+++ setup/MSBrowser.sh
@@ -60,13 +60,15 @@ case "$1" in
 	;;
   restart|reload|force-reload)
 	sudo kill -9 `ps -ef | grep MSBrowser.py |grep -v grep| cut -c11-16`
-    rm -f /var/lock/{msbrowser,ili9341}
+    rm -f /var/lock/msbrowser
+    rm -f /var/lock/ili9341
     do_start
 	exit 3
 	;;
   stop)
 	sudo kill -9 `ps -ef | grep MSBrowser.py |grep -v grep| cut -c11-16`
-    rm -f /var/lock/{msbrowser,ili9341}
+    rm -f /var/lock/msbrowser
+    rm -f /var/lock/ili9341
 	;;
   status)
 	do_status
