79b44268ff7c38fb9ac3fcf9ea48f5a1bd235a53
Seth T <seth10@users.noreply.github.com>
2017-06-30 09:30:33 -0400

    Finish 01-Change_Hostname.py

--- programs/01-Change_Hostname.py
+++ programs/01-Change_Hostname.py
@@ -23,7 +23,6 @@
 # Date          Author          Comments
 # June 2017     Seth Tenembaum  Initial Authoring
 
-
 from PiStorms import PiStorms
 from TouchScreenInput import TouchScreenInput
 import subprocess
@@ -35,17 +34,21 @@ def setHostname(hostname):
     subprocess.call("sudo sed -i 's/^127\.0\.1\.1.*$/127\.0\.1\.1       \"%s\"/' /etc/hosts" % hostname, shell=True)
     subprocess.call("echo \"%s\" | sudo tee /etc/hostname > /dev/null" % hostname, shell=True)
     return subprocess.call("sudo /etc/init.d/hostname.sh", shell=True) == 0
-    
-while psm.screen.askYesOrNoQuestion(["Change Hostname", "Would you like to set the system hostname?"], wrapText=True):
+
+message = ["Change Hostname",
+           "Would you like to set the system hostname?",
+           "If so, please use only lower-case or capital letters." # no comma so string joins with following line
+           "You may also use a period or dash between any pair of letters."]
+while psm.screen.askYesOrNoQuestion(message, wrapText=True):
     userInput = keyboard.getInput()
     hostname = userInput['response']
     if hostname == '' or not userInput['submitted']:
         continue
     if setHostname(hostname):
-        psm.screen.showMessage(["Success!", "Your hostname was successfully changed to %s." % hostname], wrapText=True)
-        # TODO: restart browser to reflect new hostname
+        psm.screen.showMessage(["Success!", 'Your hostname was successfully changed to "%s".' % hostname], wrapText=True)
+        # restart browser to reflect new hostname (shown as a cyan title at the top of the screen)
+        subprocess.call("sudo /etc/init.d/MSBrowser.sh restart", shell=True)
         break
     else:
-        psm.screen.showMessage(["Failure", "Unable to set hostname."], wrapText=True)
-# Please use only lower-case or capital letters. You may also use a period or dash between any pair of letters.
+        psm.screen.showMessage(["Failure", 'Unable to set hostname to "%s".' % hostname], wrapText=True)
 
