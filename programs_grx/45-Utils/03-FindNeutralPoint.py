45dfa67ba41bfb487fd7485b24230b7233156f62
Seth T <seth10@users.noreply.github.com>
2017-07-18 16:30:28 -0400

    Add a simple automatic mode for finding the neutral point
    Still need to actually write these to file and use them in RCServo
    constructions.

--- programs_grx/45-Utils/03-FindNeutralPoint.py
+++ programs_grx/45-Utils/03-FindNeutralPoint.py
@@ -1,30 +1,44 @@
 import time
-
-from PiStorms_GRX import PiStorms_GRX, RCServo, RCServoEncoder
+from PiStormsCom_GRX import GRXCom
+from PiStorms_GRX import PiStorms_GRX, RCServo, GrovePort
 psm = PiStorms_GRX()
 
 # introduce program and intent
 # give option to quit
-bank = psm.screen.askQuestion(["Motor Bank", "Which bank is your servo connected to?"], ["<-- B", "A -->"], wrapText=True)
+bank = psm.screen.askQuestion(["Motor Bank", "Which bank is your servo connected to?"], [" <-- B", " "*13+"A -->"], wrapText=True)
 bank = ["B", "A"][bank]
 port = psm.screen.askQuestion(["Motor Port", "Which port number is your servo connected to?"], [1, 2, 3], wrapText=True)
 port = ["1", "2", "3"][port]
 servo = "B{}M{}".format(bank, port)
-print(servo)
+#print(servo)
+servo = RCServo(servo)
+#time.sleep(0.5)
+#servo.stop()
 
 if psm.screen.askYesOrNoQuestion(["Encoder", "Do you have an encoder attached to this servo?"], wrapText=True):
     port = psm.screen.askQuestion(["Encoder Port", "Which port is your encoder connected to?"],
-            ["B1D{}".format(bank), "B{}D2".format(bank)], wrapText=True)
+            ["B{}D1".format(bank), "B{}D2".format(bank)], wrapText=True)
     port = ["1", "2"][port]
     encoder = "B{}D{}".format(bank, port)
-    servo = RCServoEncoder(port=servo, encoder=encoder)
+    encoder = GrovePort(encoder, type=GRXCom.TYPE.ENCODER, mode=int(port))
     # maybe add option to cancel here
+    # no comma at the end of this line so the string merges with the following line (no backslash necessary)
     psm.screen.showMessage(["Ready to begin", "This program will now find the neutral point of this servo automatically."
                             " Please make sure the servo can spin freely before proceeding."], wrapText=True)
-    servo.setPulse()
-    servo.readEncoder()
+    pulse = 1500
+    initialKeyPressCount = psm.getKeyPressCount()
+    while psm.getKeyPressCount() == initialKeyPressCount:
+        servo.setPulse(pulse)
+        encoderStart = encoder.readValue()
+        time.sleep(1)
+        encoderEnd = encoder.readValue()
+        encoderDifference = encoderEnd - encoderStart
+        psm.screen.termReplaceLastLine(encoderDifference)
+        if abs(encoderDifference) < 2:
+            break
+        else:
+            pulse += encoderDifference*2
 else:
-    servo = RCServo(servo)
     psm.screen.termPrintln("Please adjust this number")
     psm.screen.termPrintln("until the servo stops spinning.")
     psm.screen.termPrintln("(press GO to save and quit)")
@@ -47,5 +61,7 @@ else:
         psm.screen.drawDisplay(pulse)
         servo.setPulse(pulse)
         time.sleep(0.3)
+    #TODO: account for possibility of a wider range of values which cause servo to still
     #TODO: save value
 
+servo.stop()
