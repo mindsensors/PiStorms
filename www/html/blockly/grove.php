6f19c8469260157e37ad176ed0cc18298312cfc0
Seth T <seth10@users.noreply.github.com>
2017-07-19 16:45:53 -0400

    Finish adding Grove sensors blocks to Blockly

--- www/html/blockly/grove.php
+++ www/html/blockly/grove.php
@@ -37,6 +37,44 @@
   <block type="Grove_Flame_Sensor"></block>
   <sep gap="5"></sep>
   <block type="Grove_Flame_Sensor__fireDetected"></block>
+
+  <block type="Grove_Luminance_Sensor"></block>
+  <sep gap="5"></sep>
+  <block type="Grove_Luminance_Sensor__luminance"></block>
+
+  <block type="Grove_Light_Sensor"></block>
+  <sep gap="5"></sep>
+  <block type="Grove_Light_Sensor__lightLevel"></block>
+
+  <block type="Grove_Temperature_Sensor"></block>
+  <sep gap="5"></sep>
+  <block type="Grove_Temperature_Sensor__temperature"></block>
+  <sep gap="5"></sep>
+  <block type="Grove_Temperature_Sensor__CtoF"></block>
+
+  <block type="Grove_UV_Sensor"></block>
+  <sep gap="5"></sep>
+  <block type="Grove_UV_Sensor__intensity"></block>
+  <sep gap="5"></sep>
+  <block type="Grove_UV_Sensor__UVindex"></block>
+
+  <block type="Grove_Moisture_Sensor"></block>
+  <sep gap="5"></sep>
+  <block type="Grove_Moisture_Sensor__moistureLevel"></block>
+
+  <block type="Grove_Sound_Sensor"></block>
+  <sep gap="5"></sep>
+  <block type="Grove_Sound_Sensor__soundIntensity"></block>
+
+  <block type="Grove_Loudness_Sensor"></block>
+  <sep gap="5"></sep>
+  <block type="Grove_Loudness_Sensor__detectSound"></block>
+
+  <block type="Grove_Air_Quality_Sensor"></block>
+  <sep gap="5"></sep>
+  <block type="Grove_Air_Quality_Sensor__airQuality"></block>
+  <sep gap="5"></sep>
+  <block type="Grove_Air_Quality_Sensor__qualitativeMeasurement"></block>
 </category>
 
 
@@ -100,7 +138,7 @@ makeBlock('Grove_PIR_Motion_Sensor',
             .appendField(new Blockly.FieldDropdown(allPorts), "port");
         this.setInputsInline(true);
         this.setOutput(true, "Grove_PIR_Motion_Sensor");
-        this.setColour(0);
+        this.setColour(20);
         this.setTooltip("");
         this.setHelpUrl("");
     },
@@ -122,7 +160,7 @@ makeBlock('Grove_PIR_Motion_Sensor__motionDetected',
             .appendField("detect motion");
         this.setInputsInline(true);
         this.setOutput(true, "Boolean");
-        this.setColour(0);
+        this.setColour(20);
         this.setTooltip("");
         this.setHelpUrl("");
     },
@@ -131,5 +169,406 @@ makeBlock('Grove_PIR_Motion_Sensor__motionDetected',
         return [`${sensor}.motionDetected()`, Blockly.Python.ORDER_ATOMIC];
     }
 );
-</script>
 
+
+makeBlock('Grove_Flame_Sensor',
+    function() {
+        this.appendDummyInput()
+            .appendField("Grove flame sensor at")
+            .appendField(new Blockly.FieldDropdown(allPorts), "port");
+        this.setInputsInline(true);
+        this.setOutput(true, "Grove_Flame_Sensor");
+        this.setColour(40);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var port = block.getFieldValue('port');
+        Blockly.Python.definitions_.import_GroveDevices = "import GroveDevices";
+        Blockly.Python.definitions_[`groveFlameSensor_${port}`] = `groveFlameSensor_${port} = GroveDevices.Grove_Flame_Sensor("${port}")`;
+        return [`groveFlameSensor_${port}`, Blockly.Python.ORDER_ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Flame_Sensor__fireDetected',
+    function() {
+        this.appendValueInput("sensor")
+            .setCheck("Grove_Flame_Sensor")
+            .appendField("does");
+        this.appendDummyInput()
+            .appendField("detect fire");
+        this.setInputsInline(true);
+        this.setOutput(true, "Boolean");
+        this.setColour(40);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var sensor = Blockly.Python.valueToCode(block, 'sensor', Blockly.Python.ORDER_ATOMIC);
+        return [`${sensor}.fireDetected()`, Blockly.Python.ORDER_ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Luminance_Sensor',
+    function() {
+        this.appendDummyInput()
+            .appendField("Grove luminance sensor at")
+            .appendField(new Blockly.FieldDropdown(analogPorts), "port");
+        this.setInputsInline(true);
+        this.setOutput(true, "Grove_Luminance_Sensor");
+        this.setColour(60);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var port = block.getFieldValue('port');
+        Blockly.Python.definitions_.import_GroveDevices = "import GroveDevices";
+        Blockly.Python.definitions_[`groveLuminanceSensor_${port}`] = `groveLuminanceSensor_${port} = GroveDevices.Grove_Luminance_Sensor("${port}")`;
+        return [`groveLuminanceSensor_${port}`, Blockly.Python.ORDER_ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Luminance_Sensor__luminance',
+    function() {
+        this.appendValueInput("sensor")
+            .setCheck("Grove_Luminance_Sensor")
+            .appendField("luminance of");
+        this.setInputsInline(true);
+        this.setOutput(true, "Number");
+        this.setColour(60);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var sensor = Blockly.Python.valueToCode(block, 'sensor', Blockly.Python.ORDER_ATOMIC);
+        return [`${sensor}.luminance()`, Blockly.Python.ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Light_Sensor',
+    function() {
+        this.appendDummyInput()
+            .appendField("Grove light sensor at")
+            .appendField(new Blockly.FieldDropdown(analogPorts), "port");
+        this.setInputsInline(true);
+        this.setOutput(true, "Grove_Light_Sensor");
+        this.setColour(80);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var port = block.getFieldValue('port');
+        Blockly.Python.definitions_.import_GroveDevices = "import GroveDevices";
+        Blockly.Python.definitions_[`groveLightSensor_${port}`] = `groveLightSensor_${port} = GroveDevices.Grove_Light_Sensor("${port}")`;
+        return [`groveLightSensor_${port}`, Blockly.Python.ORDER_ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Light_Sensor__lightLevel',
+    function() {
+        this.appendValueInput("sensor")
+            .setCheck("Grove_Light_Sensor")
+            .appendField("light level of");
+        this.setInputsInline(true);
+        this.setOutput(true, "Number");
+        this.setColour(80);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var sensor = Blockly.Python.valueToCode(block, 'sensor', Blockly.Python.ORDER_ATOMIC);
+        return [`${sensor}.lightLevel()`, Blockly.Python.ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Temperature_Sensor',
+    function() {
+        this.appendDummyInput()
+            .appendField("Grove temperature sensor at")
+            .appendField(new Blockly.FieldDropdown(analogPorts), "port");
+        this.setInputsInline(true);
+        this.setOutput(true, "Grove_Temperature_Sensor");
+        this.setColour(100);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var port = block.getFieldValue('port');
+        Blockly.Python.definitions_.import_GroveDevices = "import GroveDevices";
+        Blockly.Python.definitions_[`groveTemperatureSensor_${port}`] = `groveTemperatureSensor_${port} = GroveDevices.Grove_Temperature_Sensor("${port}")`;
+        return [`groveTemperatureSensor_${port}`, Blockly.Python.ORDER_ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Temperature_Sensor__temperature',
+    function() {
+        this.appendValueInput("sensor")
+            .setCheck("Grove_Temperature_Sensor")
+            .appendField("detected temperature of");
+        this.setInputsInline(true);
+        this.setOutput(true, "Number");
+        this.setColour(100);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var sensor = Blockly.Python.valueToCode(block, 'sensor', Blockly.Python.ORDER_ATOMIC);
+        return [`${sensor}.temperature()`, Blockly.Python.ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Temperature_Sensor__CtoF',
+    function() {
+        this.appendValueInput("celsius")
+            .setCheck("Number")
+            .appendField("Farenheit conversion of")
+        this.appendDummyInput()
+            .appendField("Celsius")
+        this.setInputsInline(true);
+        this.setOutput(true, "Number");
+        this.setColour(100);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        Blockly.Python.definitions_.import_GroveDevices = "import GroveDevices";
+        var celsius = Blockly.Python.valueToCode(block, 'celsius', Blockly.Python.ORDER_ATOMIC);
+        return [`GroveDevices.Grove_Temperature_Sensor.CtoF(${celsius})`, Blockly.Python.ATOMIC];
+
+    }
+);
+
+
+makeBlock('Grove_UV_Sensor',
+    function() {
+        this.appendDummyInput()
+            .appendField("Grove UV sensor at")
+            .appendField(new Blockly.FieldDropdown(analogPorts), "port");
+        this.setInputsInline(true);
+        this.setOutput(true, "Grove_UV_Sensor");
+        this.setColour(120);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var port = block.getFieldValue('port');
+        Blockly.Python.definitions_.import_GroveDevices = "import GroveDevices";
+        Blockly.Python.definitions_[`groveUVSensor_${port}`] = `groveUVSensor_${port} = GroveDevices.Grove_UV_Sensor("${port}")`;
+        return [`groveUVSensor_${port}`, Blockly.Python.ORDER_ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_UV_Sensor__intensity',
+    function() {
+        this.appendValueInput("sensor")
+            .setCheck("Grove_UV_Sensor")
+            .appendField("measured intensity of");
+        this.setInputsInline(true);
+        this.setOutput(true, "Number");
+        this.setColour(120);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var sensor = Blockly.Python.valueToCode(block, 'sensor', Blockly.Python.ORDER_ATOMIC);
+        return [`${sensor}.intensity()`, Blockly.Python.ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_UV_Sensor__UVindex',
+    function() {
+        this.appendValueInput("sensor")
+            .setCheck("Grove_UV_Sensor")
+            .appendField("UV index of");
+        this.setInputsInline(true);
+        this.setOutput(true, "Number");
+        this.setColour(120);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var sensor = Blockly.Python.valueToCode(block, 'sensor', Blockly.Python.ORDER_ATOMIC);
+        return [`${sensor}.UVindex()`, Blockly.Python.ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Moisture_Sensor',
+    function() {
+        this.appendDummyInput()
+            .appendField("Grove moisture sensor at")
+            .appendField(new Blockly.FieldDropdown(analogPorts), "port");
+        this.setInputsInline(true);
+        this.setOutput(true, "Grove_Moisture_Sensor");
+        this.setColour(140);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var port = block.getFieldValue('port');
+        Blockly.Python.definitions_.import_GroveDevices = "import GroveDevices";
+        Blockly.Python.definitions_[`groveMoistureSensor_${port}`] = `groveMoistureSensor_${port} = GroveDevices.Grove_Moisture_Sensor("${port}")`;
+        return [`groveMoistureSensor_${port}`, Blockly.Python.ORDER_ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Moisture_Sensor__moistureLevel',
+    function() {
+        this.appendValueInput("sensor")
+            .setCheck("Grove_Moisture_Sensor")
+            .appendField("moisture level of");
+        this.setInputsInline(true);
+        this.setOutput(true, "Number");
+        this.setColour(140);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var sensor = Blockly.Python.valueToCode(block, 'sensor', Blockly.Python.ORDER_ATOMIC);
+        return [`${sensor}.moistureLevel()`, Blockly.Python.ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Sound_Sensor',
+    function() {
+        this.appendDummyInput()
+            .appendField("Grove sound sensor at")
+            .appendField(new Blockly.FieldDropdown(analogPorts), "port");
+        this.setInputsInline(true);
+        this.setOutput(true, "Grove_Sound_Sensor");
+        this.setColour(160);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var port = block.getFieldValue('port');
+        Blockly.Python.definitions_.import_GroveDevices = "import GroveDevices";
+        Blockly.Python.definitions_[`groveSoundSensor_${port}`] = `groveSoundSensor_${port} = GroveDevices.Grove_Sound_Sensor("${port}")`;
+        return [`groveSoundSensor_${port}`, Blockly.Python.ORDER_ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Sound_Sensor__soundIntensity',
+    function() {
+        this.appendValueInput("sensor")
+            .setCheck("Grove_Sound_Sensor")
+            .appendField("measured sound intensity of");
+        this.setInputsInline(true);
+        this.setOutput(true, "Number");
+        this.setColour(160);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var sensor = Blockly.Python.valueToCode(block, 'sensor', Blockly.Python.ORDER_ATOMIC);
+        return [`${sensor}.soundIntensity()`, Blockly.Python.ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Loudness_Sensor',
+    function() {
+        this.appendDummyInput()
+            .appendField("Grove loudness sensor at")
+            .appendField(new Blockly.FieldDropdown(analogPorts), "port");
+        this.setInputsInline(true);
+        this.setOutput(true, "Grove_Loudness_Sensor");
+        this.setColour(180);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var port = block.getFieldValue('port');
+        Blockly.Python.definitions_.import_GroveDevices = "import GroveDevices";
+        Blockly.Python.definitions_[`groveLoudnesSensor_${port}`] = `groveLoudnessSensor_${port} = GroveDevices.Grove_Loudness_Sensor("${port}")`;
+        return [`groveLoudnessSensor_${port}`, Blockly.Python.ORDER_ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Loudness_Sensor__detectSound',
+    function() {
+        this.appendValueInput("sensor")
+            .setCheck("Grove_Loudness_Sensor")
+            .appendField("detect sound of");
+        this.setInputsInline(true);
+        this.setOutput(true, "Number");
+        this.setColour(180);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var sensor = Blockly.Python.valueToCode(block, 'sensor', Blockly.Python.ORDER_ATOMIC);
+        return [`${sensor}.detectSound()`, Blockly.Python.ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Air_Quality_Sensor',
+    function() {
+        this.appendDummyInput()
+            .appendField("Grove air quality sensor at")
+            .appendField(new Blockly.FieldDropdown(analogPorts), "port");
+        this.setInputsInline(true);
+        this.setOutput(true, "Grove_Air_Quality_Sensor");
+        this.setColour(200);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var port = block.getFieldValue('port');
+        Blockly.Python.definitions_.import_GroveDevices = "import GroveDevices";
+        Blockly.Python.definitions_[`groveAirQualitySensor_${port}`] = `groveAirQualitySensor_${port} = GroveDevices.Grove_Air_Quality_Sensor("${port}")`;
+        return [`groveAirQualitySensor_${port}`, Blockly.Python.ORDER_ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Air_Quality_Sensor__airQuality',
+    function() {
+        this.appendValueInput("sensor")
+            .setCheck("Grove_Air_Quality_Sensor")
+            .appendField("measured air quality of");
+        this.setInputsInline(true);
+        this.setOutput(true, "Number");
+        this.setColour(200);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var sensor = Blockly.Python.valueToCode(block, 'sensor', Blockly.Python.ORDER_ATOMIC);
+        return [`${sensor}.airQuality()`, Blockly.Python.ATOMIC];
+    }
+);
+
+
+makeBlock('Grove_Air_Quality_Sensor__qualitativeMeasurement',
+    function() {
+        this.appendValueInput("sensor")
+            .setCheck("Grove_Air_Quality_Sensor")
+            .appendField("air quality description from");
+        this.setInputsInline(true);
+        this.setOutput(true, "Number");
+        this.setColour(200);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    },
+    function(block) {
+        var sensor = Blockly.Python.valueToCode(block, 'sensor', Blockly.Python.ORDER_ATOMIC);
+        return [`${sensor}.airQuality()`, Blockly.Python.ATOMIC];
+    }
+);
+</script>
