f6b07e6a64f9c0277b8920a560abf148240dab5c
Seth T <seth10@users.noreply.github.com>
2017-07-24 13:38:47 -0400

    Update 01-TestInternetConn for GRX

--- programs_grx/01-TestInternetConn.py
+++ programs_grx/01-TestInternetConn.py
@@ -22,50 +22,37 @@
 # History:
 # Date          Author          Comments
 # March 2016    Roman Bohuk     Initial Authoring
+# July 2017     Seth Tenembaum  Update for GRX
 
-# Setup (to be present in all programs)
-import os,sys,inspect,time,thread
-import socket,fcntl,struct
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir)
+import time, socket
+from PiStorms_GRX import PiStorms_GRX
+psm = PiStorms_GRX()
 
-from PiStorms import PiStorms
-import socket
-
-# Globals
-psm = PiStorms()
-
-# Checks if internet is available
-def available():
+def checkInternet():
     try:
         socket.setdefaulttimeout(5)
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("8.8.8.8", 53))
         return True
-    except Exception as e: pass
-    return False
+    except:
+        return False
 
-psm.screen.fillRect(0, 0, 320, 240, fill = (0,0,0), display = False)
-psm.screen.drawAutoText("Testing internet connection", 20, 30, fill = (255,255,255), size = 23, display = False)
-psm.screen.fillRect(20, 80, 320, 240, fill = (0,0,0), display = False)
-psm.screen.fillBmp(110, 110, 100, 100, path = currentdir+'/'+'load.png', display = False)
-psm.screen.fillRect(0, 0, 1, 1, fill = (0,0,0), display = True)
+psm.screen.drawAutoText("Testing internet connection", 20, 30, fill=(255,255,255), size=23, display=False)
+psm.screen.fillRect(20, 80, 320, 240, fill=(0,0,0), display=False)
+psm.screen.fillBmp(110, 110, 100, 100, path='load.png', display=False)
+psm.screen.disp.display()
+#time.sleep(1)
 
+c = checkInternet()
+color = (0,255,0) if c else (255,0,0)
+message1 = "You are connected" if c else "You are not connected"
+image = "wifi_green.png" if c else "x_red.png"
 
-test = available()
-psm.screen.fillRect(0, 0, 320, 240, fill = (0,0,0), display = False)
-if test:
-    psm.screen.drawAutoText("You are connected", 35, 20, fill = (0,255,0), size = 25, display = False)
-    psm.screen.drawAutoText("to the internet!", 35, 50, fill = (0,255,0), size = 25, display = False)
-    psm.screen.fillBmp(120, 85, 80, 80, path = currentdir+'/'+'wifi_green.png', display = False)
-else:
-    psm.screen.drawAutoText("You are not connected", 35, 20, fill = (255,0,0), size = 25, display = False)
-    psm.screen.drawAutoText("to the internet!", 35, 50, fill = (255,0,0), size = 25, display = False)
-    psm.screen.fillBmp(130, 95, 60, 60, path = currentdir+'/'+'x_red.png', display = False)
+psm.screen.clearScreen(display=False)
+psm.screen.drawAutoText(message1, 35, 20, fill=color, size=25, display=False)
+psm.screen.drawAutoText("to the internet!", 35, 50, fill=color, size=25, display=False)
+psm.screen.fillBmp(120, 85, 80, 80, path=image, display=False)
 
-psm.screen.drawButton(35, 170, width = 250, height = 40, text="Continue", display=False)
-psm.screen.fillRect(0, 0, 1, 1, fill = (0,0,0), display = True)
-exit = False
-while not exit:
-    cont = psm.screen.checkButton(35, 170, 250, 40)
-    if cont or psm.isKeyPressed(): exit = True
+psm.screen.drawButton(35, 170, width=250, height=40, text="Continue", display=False)
+psm.screen.disp.display()
+
+psm.untilKeyPressOrTouch(time.sleep, 0.01)
