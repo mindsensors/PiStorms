edcdb64434bbecb00d4164fb4ace956522ebb9de
Seth T <seth10@users.noreply.github.com>
2017-07-27 15:22:46 -0400

    Add descriptive error message for Joystick program

--- programs_grx/60-Robots/01-Joystick.py
+++ programs_grx/60-Robots/01-Joystick.py
@@ -5,10 +5,14 @@ from PiStorms_GRX import RCServo
 l = RCServo("BAS1", 1300)
 r = RCServo("BAS2", 1690)
 
-import pygame
-pygame.init()
-joystick = pygame.joystick.Joystick(0)
-joystick.init()
+import pygame, sys
+try:
+    pygame.init()
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+except:
+    psm.screen.showMessage("Please connect a joystick and try again.")
+    sys.exit(0)
 
 import time
 while not psm.isKeyPressed() and not joystick.get_button(16):
