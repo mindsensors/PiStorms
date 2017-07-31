11f13d338018ddf55192a0b06de2fc2a049720f6
Seth T <seth10@users.noreply.github.com>
2017-07-31 12:42:10 -0400

    Add copyright statement where missing

--- sys/pistorms-diag.sh
+++ sys/pistorms-diag.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+#
+# Copyright (c) 2017 mindsensors.com
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+#mindsensors.com invests time and resources providing this open source code,
+#please support mindsensors.com  by purchasing products from mindsensors.com!
+#Learn more product option visit us @  http://www.mindsensors.com/
 
 echo ""
 echo "config file"
