63e2b5f63717a4221f0f4597b9b53edf9c9e61ed
mindsensors.com <info@mindsensors.com>
2019-12-01 21:03:27 -0500

    Update mindsensorsUI.py

--- sys/mindsensorsUI.py
+++ sys/mindsensorsUI.py
@@ -32,7 +32,7 @@
 from PiStormsCom import PiStormsCom
 from PiStormsCom_GRX import GRXCom
 import time, os, sys, math
-form PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont
 import textwrap
 import MS_ILI9341 as TFT
 from Adafruit_ILI9341 import ILI9341_INVOFF
