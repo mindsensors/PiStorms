c09deb13236ee88e4a5daa6595135c4682e4fc6b
mindsensors.com <info@mindsensors.com>
2019-12-07 23:04:38 -0500

    Update 01-PopHeadsLive.py

--- programs/50-CameraDemos/01-PopHeadsLive.py
+++ programs/50-CameraDemos/01-PopHeadsLive.py
@@ -90,7 +90,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     scaleFactor=1.1,
     minNeighbors=5,
     minSize=(30, 30),
-    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+    flags = cv2.CASCADE_SCALE_IMAGE
     )
 
     #print benchmarking information on Console
